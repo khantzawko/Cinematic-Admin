7507a38026e7e381925e040ad719f92afe9bc2a9
Khant Zaw Ko <khantzawko7@gmail.com>
2018-01-18 12:23:17 +0630

    Initial Commit

---
+++ Cinematic AdminTests/Cinematic_AdminTests.swift
@@ -0,0 +1,36 @@
+//
+//  Cinematic_AdminTests.swift
+//  Cinematic AdminTests
+//
+//  Created by Khant Zaw Ko on 18/1/18.
+//  Copyright © 2018 Khant Zaw Ko. All rights reserved.
+//
+
+import XCTest
+@testable import Cinematic_Admin
+
+class Cinematic_AdminTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
